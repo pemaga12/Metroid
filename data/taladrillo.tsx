6441f46672116124069d5ea1459143398c45400c
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-05-18 11:58:14 +0200

    Primera iteración del nivel 1

---
+++ data/taladrillo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="taladrillo" tilewidth="16" tileheight="24" tilecount="16" columns="16">
+ <image source="../images/Enemys/taladrillo.png" width="270" height="24"/>
+</tileset>
---
+++ data/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tiles" tilewidth="16" tileheight="16" tilecount="54" columns="9">
+ <image source="../images/tiles_metroid_!6x16.png" width="144" height="96"/>
+</tileset>
