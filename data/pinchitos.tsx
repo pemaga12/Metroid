a5097a8001e57cb59d9dd533015158fdc8aab76c
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-05-18 16:37:34 +0200

    Añadidos pinchitos

---
+++ data/pinchitos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="pinchitos" tilewidth="17" tileheight="16" tilecount="33" columns="33">
+ <image source="../images/Enemys/pinchitos.png" width="576" height="16"/>
+</tileset>
