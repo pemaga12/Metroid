6d47c7e51e33f66fe06df5eb0d0e88e14890a974
Pedro Martínez Gamero <47608569+pemaga12@users.noreply.github.com>
2021-05-19 14:47:35 +0200

    Añadidos mas sprites

---
+++ data/medusas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="medusas" tilewidth="23" tileheight="24" spacing="2" tilecount="6" columns="6">
+ <image source="../images/Enemys/medusas.png" width="150" height="24"/>
+</tileset>
---
+++ data/navecitas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="navecitas" tilewidth="16" tileheight="16" spacing="2" tilecount="9" columns="9">
+ <image source="../images/Enemys/navecitas.png" width="162" height="16"/>
+</tileset>
